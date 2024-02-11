cd34568e325a8ea59bfdbac5d1d31cf97d2f5ddb
Julien Miquel <86713160+julienmiquel@users.noreply.github.com>
2024-02-11 20:58:49 +0000

    change projectid

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-PROJECT_ID = os.environ.get("GCP_PROJECT", "customer-demo-01")  # Your Google Cloud Project ID
+PROJECT_ID = os.environ.get("GCP_PROJECT", "ml-demo-384110")  # Your Google Cloud Project ID
 LOCATION = os.environ.get("GCP_REGION", "us-central1")  # Your Google Cloud Project Region
 
 REGION = LOCATION
